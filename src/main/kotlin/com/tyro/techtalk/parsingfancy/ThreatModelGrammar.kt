ac1baa7ede7404c775548de40cbeaf54cec4eb0f
Gihan Fernando and Simon Gerber <just-dev+gfernando+sgerber@tyro.com>
2017-11-24 13:30:14 +1100

    Implement rest of parser

--- src/main/kotlin/com/tyro/techtalk/parsingfancy/ThreatModelGrammar.kt
+++ src/main/kotlin/com/tyro/techtalk/parsingfancy/ThreatModelGrammar.kt
@@ -2,32 +2,65 @@ package com.tyro.techtalk.parsingfancy
 
 import com.github.h0tk3y.betterParse.combinators.*
 import com.github.h0tk3y.betterParse.grammar.Grammar
-import com.tyro.techtalk.parsingfancy.threatmodel.CounterMeasure
+import com.tyro.techtalk.parsingfancy.threatmodel.*
 import com.tyro.techtalk.parsingfancy.threatmodel.Target
 
-object ThreatModelParser : Grammar<String>() {
+object ThreatModelParser : Grammar<List<AttackResult>>() {
     private val TARGET_HEADER by token("Targets:")
+    private val THREAT_HEADER by token("Threats:")
+    private val SCENARIO_HEADER by token("Scenarios:")
+
     private val COMMA by token(",\\s?")
     private val LBRACE by token("\\[")
     private val RBRACE by token("\\]")
+
+    private val VERSUS by token("vs")
+
     private val NO_DEFENCES by token("is undefended")
     private val DEFENSES_PREFIX by token("is defended by")
+
+    private val NO_WEAKNESSES by token("has no weaknesses")
+    private val WEAKNESSES_SINGULAR by token("is weak against")
+    private val WEAKNESSES_PLURAL by token("are weak against")
+
     private val IDENT by token("[A-Za-z\\-_]+")
 
     private val WHITESPACE by token("\\s", ignore = true)
     private val NEWLINES by token("[\r\n]", ignore = true)
 
     val oneIdentifier = IDENT use { listOf(text) }
-    val manyIdentifiers = (skip(LBRACE) and separatedTerms(IDENT, COMMA) and skip(RBRACE)) map { it.map { it.text } }
+    private val manyIdentifiers = (skip(LBRACE) and separatedTerms(IDENT, COMMA) and skip(RBRACE)) map { it.map { it.text } }
     val identifierList = oneIdentifier or manyIdentifiers
 
-    val noDefenses = NO_DEFENCES map { emptyList<String>() }
-    val someDefenses = skip(DEFENSES_PREFIX) and identifierList
-    val defences = (noDefenses or someDefenses) map { it.map { CounterMeasure(it) } }
+    private val noDefenses = NO_DEFENCES asJust emptyList<CounterMeasure>()
+    private val someDefenses = skip(DEFENSES_PREFIX) and identifierList map { it.map { CounterMeasure(it) } }
+    private val defences = (noDefenses or someDefenses)
 
-    val target = IDENT and defences use { Target(t1.text, t2.toSet()) }
-    val targets = separatedTerms(target, WHITESPACE)
+    private val target = IDENT and defences use { Target(t1.text, t2.toSet()) }
+    private val targets = separatedTerms(target, WHITESPACE)
     val targetSection = skip(TARGET_HEADER) and targets
 
-    override val rootParser = IDENT use { text }
+    private val noWeaknesses = NO_WEAKNESSES asJust emptyList<CounterMeasure>()
+    private val someWeaknesses = skip(WEAKNESSES_PLURAL or WEAKNESSES_SINGULAR) and identifierList map { it.map { CounterMeasure(it) } }
+    private val weaknesses = noWeaknesses or someWeaknesses
+
+    private val threat = IDENT and weaknesses use { Threat(t1.text, t2.toSet()) }
+    private val threats = separatedTerms(threat, WHITESPACE)
+    val threatSection = skip(THREAT_HEADER) and threats
+
+    private val scenario = IDENT and skip(VERSUS) and IDENT map { Pair(it.t1.text, it.t2.text) }
+    private val scenarios = separatedTerms(scenario, WHITESPACE)
+    val scenarioSection = skip(SCENARIO_HEADER) and scenarios
+
+    override val rootParser by targetSection and threatSection and scenarioSection map { (targets, threats, scenarios) ->
+        val targetMap = targets.map { it.name to it }.toMap()
+        val threatMap = threats.map { it.name to it }.toMap()
+
+        scenarios.map { (threatName, targetName) ->
+            val target = targetMap.getOrDefault(targetName, Target(targetName))
+            val threat = threatMap.getOrDefault(threatName, Threat(threatName))
+
+            AttackScenario.modelAttack(target, threat)
+        }
+    }
 }
--- src/test/kotlin/com/tyro/techtalk/parsingfancy/ThreatModelParserTest.kt
+++ src/test/kotlin/com/tyro/techtalk/parsingfancy/ThreatModelParserTest.kt
@@ -1,20 +1,48 @@
 package com.tyro.techtalk.parsingfancy
 
+import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import com.github.h0tk3y.betterParse.parser.parseToEnd
+import com.github.h0tk3y.betterParse.utils.Tuple2
+import com.tyro.techtalk.parsingfancy.threatmodel.Compromised
 import com.tyro.techtalk.parsingfancy.threatmodel.Target
+import com.tyro.techtalk.parsingfancy.threatmodel.Threat
+import com.tyro.techtalk.parsingfancy.threatmodel.UnCompromised
 import io.kotlintest.TestCaseConfig
 import io.kotlintest.matchers.contain
 import io.kotlintest.matchers.containsInOrder
 import io.kotlintest.matchers.should
+import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.FreeSpec
 
 class ThreatModelParserTest : FreeSpec() {
     private val tokenizer = ThreatModelParser.tokenizer
     private val targetFragment =
             """Targets:
-               |    Tyro is defended by jost
+               |    Tyro is defended by [jost, obscurity]
                |    TheVault is undefended
             """.trimMargin()
+
+    private val threatFragment =
+            """Threats:
+               |    Hackers are weak against obscurity
+               |    GlobalWarming has no weaknesses
+               |    Kotlin is weak against [Scala, Haskell]
+               """.trimMargin()
+
+    private val scenarioFragment =
+            """Scenarios:
+               | Hackers vs Tyro
+               | GlobalWarming vs Tyro
+               """.trimMargin()
+
+    private val entireDSL =
+            """$targetFragment
+              |
+              |$threatFragment
+              |
+              |$scenarioFragment
+              """.trimMargin()
+
 
     init {
         "The identifier list parser" - {
@@ -43,16 +71,41 @@ class ThreatModelParserTest : FreeSpec() {
             }
         }
 
-        "The threat section parser should" - {
-            "parse threats" {
+        "The target section parser should" - {
+            "parse targets" {
                 val targets = ThreatModelParser.targetSection.parseToEnd(tokenizer.tokenize(targetFragment))
 
-                targets should contain(Target("Tyro", "jost"))
+                targets should contain(Target("Tyro", "jost", "obscurity"))
                 targets should contain(Target("TheVault"))
             }
         }
-    }
 
-    override val defaultTestCaseConfig: TestCaseConfig
-        get() = super.defaultTestCaseConfig
+        "The threat parser should" - {
+            "parse threats" {
+                val threats = ThreatModelParser.threatSection.parseToEnd(tokenizer.tokenize(threatFragment))
+
+                threats should contain(Threat("Hackers", "obscurity"))
+                threats should contain(Threat("GlobalWarming"))
+                threats should contain(Threat("Kotlin", "Scala", "Haskell"))
+            }
+        }
+
+        "The scenario parser should" - {
+            "parse scenarios" {
+                val scenarios = ThreatModelParser.scenarioSection.parseToEnd(tokenizer.tokenize(scenarioFragment))
+
+                scenarios should contain(Pair("Hackers", "Tyro"))
+                scenarios should contain(Pair("GlobalWarming", "Tyro"))
+            }
+        }
+
+        "The entire grammar should" - {
+            "parse and evaluate attack scenarios" {
+                val attackScenarios = ThreatModelParser.parseToEnd(entireDSL)
+
+                attackScenarios[0] shouldBe UnCompromised
+                attackScenarios[1] shouldBe Compromised
+            }
+        }
+    }
 }
