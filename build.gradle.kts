ba188fd146655a9158095c128578a1e93c1fb4cf
Simon Gerber <sgerber@tyro.com>
2017-11-27 12:50:15 +1100

    Convert tests to junit so they don't blow up the whole test class when they hit an exception

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ version = "1.0-SNAPSHOT"
 
 buildscript {
     var kotlin_version: String by extra
-    kotlin_version = "1.1.60"
+    kotlin_version = "1.1.61"
 
     repositories {
         mavenCentral()
--- src/test/kotlin/com/tyro/techtalk/parsingfancy/ThreatModelParserTest.kt
+++ src/test/kotlin/com/tyro/techtalk/parsingfancy/ThreatModelParserTest.kt
@@ -10,9 +10,9 @@ import io.kotlintest.matchers.contain
 import io.kotlintest.matchers.containsInOrder
 import io.kotlintest.matchers.should
 import io.kotlintest.matchers.shouldBe
-import io.kotlintest.specs.FreeSpec
+import org.junit.Test
 
-class ThreatModelParserTest : FreeSpec() {
+class ThreatModelParserTest {
     private val tokenizer = ThreatModelParser.tokenizer
     private val targetFragment =
             """Targets:
@@ -42,68 +42,51 @@ class ThreatModelParserTest : FreeSpec() {
               """.trimMargin()
 
 
-    init {
-        "The identifier list parser" - {
-            "parse an identifier as a string" {
-                val result = ThreatModelParser.oneIdentifier.parseToEnd(tokenizer.tokenize("abcdef"))
+    @Test
+    fun `the identifier parser should parse a command separated list of tokens`() {
+        val results = ThreatModelParser.identifierList.parseToEnd(tokenizer.tokenize("[abc, def]"))
 
-                result should contain("abcdef")
-            }
+        results should containsInOrder("abc", "def")
+    }
 
-            "parse a list of identifiers with spaces after the comma" {
-                val results = ThreatModelParser.identifierList.parseToEnd(tokenizer.tokenize("[abc, def]"))
+    @Test
+    fun `the identifier parser should parse a single identifier as a list`() {
+        val results = ThreatModelParser.identifierList.parseToEnd(tokenizer.tokenize("abc"))
 
-                results should containsInOrder("abc", "def")
-            }
+        results should contain("abc")
+    }
 
-            "parse a list of identifiers without spaces after the comma" {
-                val results = ThreatModelParser.identifierList.parseToEnd(tokenizer.tokenize("[abc,def]"))
+    @Test
+    fun `the target parser should parse a whitespace separated list of targets`() {
+        val targets = ThreatModelParser.targetSection.parseToEnd(tokenizer.tokenize(targetFragment))
 
-                results should containsInOrder("abc", "def")
-            }
+        targets should contain(Target("TheVault"))
+        targets should contain(Target("Tyro", "jost", "obscurity"))
 
-            "parse a single identifier as a list" {
-                val results = ThreatModelParser.identifierList.parseToEnd(tokenizer.tokenize("abc"))
+    }
 
-                results should contain("abc")
-            }
-        }
+    @Test
+    fun `the threat parser should parse a whitespace separated list of threats`() {
+        val threats = ThreatModelParser.threatSection.parseToEnd(tokenizer.tokenize(threatFragment))
 
-        "The target section parser should" - {
-            "parse targets" {
-                val targets = ThreatModelParser.targetSection.parseToEnd(tokenizer.tokenize(targetFragment))
+        threats should contain(Threat("Hackers", "obscurity"))
+        threats should contain(Threat("GlobalWarming"))
+        threats should contain(Threat("Kotlin", "Scala", "Haskell"))
+    }
 
-                targets should contain(Target("Tyro", "jost", "obscurity"))
-                targets should contain(Target("TheVault"))
-            }
-        }
+    @Test
+    fun `the scenario parser should parse a whitespace separated list of scenarios`() {
+        val scenarios = ThreatModelParser.scenarioSection.parseToEnd(tokenizer.tokenize(scenarioFragment))
 
-        "The threat parser should" - {
-            "parse threats" {
-                val threats = ThreatModelParser.threatSection.parseToEnd(tokenizer.tokenize(threatFragment))
+        scenarios should contain(Pair("Hackers", "Tyro"))
+        scenarios should contain(Pair("GlobalWarming", "Tyro"))
+    }
 
-                threats should contain(Threat("Hackers", "obscurity"))
-                threats should contain(Threat("GlobalWarming"))
-                threats should contain(Threat("Kotlin", "Scala", "Haskell"))
-            }
-        }
+    @Test
+    fun `the threat model grammar should parse and evaluate threat scenarios`() {
+        val attackScenarios = ThreatModelParser.parseToEnd(entireDSL)
 
-        "The scenario parser should" - {
-            "parse scenarios" {
-                val scenarios = ThreatModelParser.scenarioSection.parseToEnd(tokenizer.tokenize(scenarioFragment))
-
-                scenarios should contain(Pair("Hackers", "Tyro"))
-                scenarios should contain(Pair("GlobalWarming", "Tyro"))
-            }
-        }
-
-        "The entire grammar should" - {
-            "parse and evaluate attack scenarios" {
-                val attackScenarios = ThreatModelParser.parseToEnd(entireDSL)
-
-                attackScenarios[0] shouldBe UnCompromised
-                attackScenarios[1] shouldBe Compromised
-            }
-        }
+        attackScenarios[0] shouldBe UnCompromised
+        attackScenarios[1] shouldBe Compromised
     }
 }
